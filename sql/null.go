2e48d9be5428ef2571c1e7660534fcb65eac7423
Ryan Fan <reg_info@qq.com>
2026-04-08 08:43:57 +0800

    fix null filters issue (#13)

--- sql/null.go
+++ sql/null.go
@@ -9,22 +9,28 @@ import (
 )
 
 func GetNullString(filters map[string]string, key string) sql.NullString {
-	if v, ok := filters[key]; ok {
-		return sql.NullString{String: v, Valid: true}
+	if len(filters) > 0 {
+		if v, ok := filters[key]; ok {
+			return sql.NullString{String: v, Valid: true}
+		}
 	}
 	return sql.NullString{}
 }
 
 func GetNullInt32(filters map[string]string, key string) sql.NullInt32 {
-	if v, ok := filters[key]; ok {
-		return sql.NullInt32{Int32: cast.ToInt32(v), Valid: true}
+	if len(filters) > 0 {
+		if v, ok := filters[key]; ok {
+			return sql.NullInt32{Int32: cast.ToInt32(v), Valid: true}
+		}
 	}
 	return sql.NullInt32{}
 }
 
 func GetNullInt64(filters map[string]string, key string) sql.NullInt64 {
-	if v, ok := filters[key]; ok {
-		return sql.NullInt64{Int64: cast.ToInt64(v), Valid: true}
+	if len(filters) > 0 {
+		if v, ok := filters[key]; ok {
+			return sql.NullInt64{Int64: cast.ToInt64(v), Valid: true}
+		}
 	}
 	return sql.NullInt64{}
 }
